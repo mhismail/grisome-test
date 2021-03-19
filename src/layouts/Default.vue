b60a0d6fb6fb93ce67813c9eabfee3af456aebc6
Mohamed Ismail <mhismail@buffalo.edu>
2021-03-19 14:21:52 -0500

    navbar test

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -4,12 +4,43 @@
       <strong>
         <g-link to="/">{{ $static.metadata.siteName }}</g-link>
       </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
+      <b-navbar class="is-fixed-top p-5 navbar" :transparent="true">
+        <template #brand>
+          <g-image
+            alt="Lightweight UI components for Vue.js based on Bulma"
+            width="180px"
+          />
+        </template>
+        <template #end>
+          <b-navbar-item>
+            <g-link class="nav__link" to="/">Home</g-link>
+          </b-navbar-item>
+          <b-navbar-item>
+            <g-link class="nav__link" to="/about/">About</g-link>
+          </b-navbar-item>
+          <b-navbar-item>
+            <g-link class="nav__link" to="/">ePD Team</g-link>
+          </b-navbar-item>
+          <b-navbar-item>
+            <g-link class="nav__link" to="/about/">Announcements</g-link>
+          </b-navbar-item>
+          <b-navbar-item>
+            <g-link class="nav__link" to="/">Services</g-link>
+          </b-navbar-item>
+
+          <b-navbar-dropdown label="More">
+            <b-navbar-item> Training Courses </b-navbar-item>
+            <b-navbar-item href="#"> Publications </b-navbar-item>
+          </b-navbar-dropdown>
+          <b-navbar-item tag="div">
+            <div class="buttons">
+              <a class="button is-primary"> Contact A Scientist </a>
+            </div>
+          </b-navbar-item>
+        </template>
+      </b-navbar>
     </header>
-    <slot/>
+    <slot />
   </div>
 </template>
 
@@ -23,9 +54,10 @@ query {
 
 <style>
 body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
+  margin: 0;
+  padding: 0;
   line-height: 1.5;
 }
 
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,46 @@
 <template>
   <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <div class="container is-fluid">
+      <section class="section">
+        <div class="container has-text-centered">
+          <h2 class="title">Bulma + Gridsome = 💚</h2>
+          <div class="block">
+            <a href="#">
+              <img
+                src="https://bulma.dev/placeholder/pictures/bg_4-3.svg?primary=00d1b2"
+                alt
+              />
+            </a>
+          </div>
+          <div class="columns is-mobile">
+            <div class="column is-4">
+              <a href="#">
+                <img
+                  src="https://bulma.dev/placeholder/pictures/bg_4-3.svg?primary=00d1b2"
+                  alt
+                />
+              </a>
+            </div>
+            <div class="column is-4">
+              <a href="#">
+                <img
+                  src="https://bulma.dev/placeholder/pictures/bg_4-3.svg?primary=00d1b2"
+                  alt
+                />
+              </a>
+            </div>
+            <div class="column is-4">
+              <a href="#">
+                <img
+                  src="https://bulma.dev/placeholder/pictures/bg_4-3.svg?primary=00d1b2"
+                  alt
+                />
+              </a>
+            </div>
+          </div>
+          <h4 class="title is-spaced is-4">Bulma is working with Gridsome!</h4>
+        </div>
+      </section>
+    </div>
   </Layout>
 </template>
-
-<script>
-export default {
-  metaInfo: {
-    title: 'Hello, world!'
-  }
-}
-</script>
-
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
